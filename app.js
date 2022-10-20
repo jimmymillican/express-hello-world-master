30c55e1143670b25ec39d149071cf5999e64b619
James Millican <bh.james.millican@broadlandgroup.org>
2022-10-20 22:29:13 +0100

    Your comment

--- app.js
+++ app.js
@@ -1,6 +1,78 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+
+
+const app = express()
+
+axios('https://charge.pod-point.com/address/tesco-extra-prescot-1j65m')
+    .then( response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const chargers = []
+
+
+        $('.list-group-item', html).each(function(){
+
+            // const thePodNumber = $(this).find('.door').text()
+            
+            //const thetext = $(this).text()
+     
+           //$('.title')[0].childNodes[0].nodeValue
+            const thetext2 = $(this).text()
+ 
+            chargers.fill(thetext2)
+            const ChargerName = $(this).find('h4').text().trim()
+
+            const Details = $(this).find('.details').text()
+ 
+          //  console.log('Pod : ' + ChargerName )
+
+            //console.log(' Details ' + Details )
+            const textAttributes = thetext2.split('/n')
+ 
+            // Reference to our element
+  
+            var words = Details.split(' ');
+            const ChargerArray = []
+
+            for (var i = 0; i < words.length; i++) {
+                //if (words[i].includes('Connector'))
+               // {
+                if (words[i] !== '' && words[i] !== '\n' && words[i]  ){
+                  
+                  ChargerArray.push(words[i])
+                  
+                }
+            }
+
+            for (var i = 0; i < ChargerArray.length; i++) {
+              //if (words[i].includes('Connector'))
+             // {
+              if (ChargerArray[i] == 'Connector'){
+                console.log(' Pod :  ' + ChargerName ) 
+                console.log(' Connector :  ' + ChargerArray[i + 1] ) 
+                console.log(' Status : ' + ChargerArray[i + 2] ) 
+                if (ChargerArray[i + 3] == 'Type'){
+                  console.log(' Type :  ' + ChargerArray[i + 3] + ChargerArray[i + 4] ) 
+                } else{
+                  console.log(' Type :  ' + ChargerArray[i + 3] ) 
+                }
+                
+              
+
+              }
+          
+          }
+
+         
+             }        
+         )
+  
+     }).catch(err => console.log(err))
+
 
 app.get("/", (req, res) => res.type('html').send(html));
 
